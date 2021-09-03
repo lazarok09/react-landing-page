eaafbddfdf6b7ef5299a79a5ecee220968e363b4
lazarok09 <lazarok09@gmail.com>
2021-09-03 11:17:29 -0300

    test(Menulink) testa o componente e faz inline snapshot

---
+++ src/components/MenuLink/styles.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.a`
+  ${({ theme }) => css`
+    position: relative;
+    display: block;
+    text-decoration: none;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.small};
+    color: ${theme.colors.primaryColor};
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      width: 0%;
+      height: 0.2rem;
+      background: ${theme.colors.secondaryColor};
+      transition: all 300ms ease-in-out;
+    }
+    &:hover::after {
+      left: 25%;
+      width: 50%;
+    }
+  `}
+`;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -23,5 +23,15 @@ export const theme = {
   media: {
     lteMedium: '(max-width: 768px)',
   },
+  spacings: {
+    xsmall: '0.8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
 };
 export default theme;
