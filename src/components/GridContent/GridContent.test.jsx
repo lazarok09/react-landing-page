5416ee7f10b95ddd9e9d9a0fa21ed28d1bbdae12
lazarok09 <lazarok09@gmail.com>
2021-09-12 09:34:00 -0300

    test(<GridContent />)

--- src/components/GridContent/GridContent.test.jsx
+++ src/components/GridContent/GridContent.test.jsx
@@ -2,9 +2,18 @@ import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { GridContent } from '.';
 
+import mock from './mock';
+
 describe('<GridContent />', () => {
-  it('should render', () => {
-    renderTheme(<GridContent>Children</GridContent>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render grid content', () => {
+    const { container } = renderTheme(<GridContent {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render grid content', () => {
+    const { container } = renderTheme(
+      <GridContent {...mock} background={undefined} />,
+    );
+    expect(container).toMatchSnapshot();
   });
 });
