35b5d6f5dfb0a21bfb743051180dc5f42e252cd2
lazarok09 <lazarok09@gmail.com>
2021-09-16 11:40:04 -0300

    testes finais e mapeamento de dados

--- src/api/map-sections.test.js
+++ src/api/map-sections.test.js
@@ -10,10 +10,42 @@ import {
   mapSectionTwoColumns,
   mapTextGrid,
 } from './map-sections';
+
+import pagesFakeData from './data.json';
 describe('map sections', () => {
   it('should map predefined value if no data', () => {
     const data = mapSections();
     expect(data).toEqual([]);
+  });
+  it('should render sections with correct data', () => {
+    const data = mapSections(pagesFakeData[0].sections);
+    expect(data[0].component).toBe('section.section-two-columns');
+  });
+  it('should test section with invalid data', () => {
+    const withNoTextOrImageGrid = mapSections([
+      {
+        __component: 'section.section-grid',
+      },
+    ]);
+    const withNoComponent = mapSections([{}]);
+
+    expect(withNoTextOrImageGrid).toEqual([
+      { __component: 'section.section-grid' },
+    ]);
+    expect(withNoComponent).toEqual([{}]);
+  });
+  it('should test section.section-grid with no text_grid or image_grid', () => {
+    const withNoTextOrImageGrid = mapSections([
+      {
+        __component: 'section.section-grid',
+        text_grid: [{}],
+      },
+      {
+        __component: 'section.section-grid',
+        image_grid: [{}],
+      },
+    ]);
+    expect(withNoTextOrImageGrid.length).toBe(2);
   });
 
   it('should map section two columns', () => {
