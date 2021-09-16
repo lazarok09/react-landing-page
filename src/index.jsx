e71a794a22bcbc04fe9efb45da108f9d154c3eac
lazarok09 <lazarok09@gmail.com>
2021-09-16 20:19:13 -0300

    altera home de lugar

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './templates/App/Home';
 import { GlobalStyles } from './styles/global-styles';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import Home from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Home />
       <GlobalStyles />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
